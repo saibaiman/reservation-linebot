5f5508bc917a06436171b02205fc8b1d808e50cb
EC2 Default User <ec2-user@ip-172-31-6-135.us-east-2.compute.internal>
2020-01-01 13:40:15 +0000

    edit

--- webhook.php
+++ webhook.php
@@ -23,81 +23,87 @@ $channelSecret = 'c5f2a1532069465224d1183ac4256997';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage([
-                        'replyToken' => $event['replyToken'],
-                        'messages' => [
-                            [
-                                'type' => 'text',
-                                'text' => $message['text']
-                            ]
-                        ]
-                    ]);
-                    break;
-                case 'location':
-                    $lat = $message['latitude'];
-                    $lng = $message['longitude'];
-                    $hotUrl = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e8a77202e4c8db72&lat=' . $lat . '&lng=' . $lng . '&range=5&order=4&format=json';
-
-                    $ch = curl_init();
-                    curl_setopt($ch, CURLOPT_URL, $hotUrl);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    $result = curl_exec($ch);
-                    curl_close($ch);
-                    $data = json_decode($result, true);
-                    if (!empty($data['results']['shop'])) {
-		    	$shopInfo0 = $data['results']['shop']['0']['name'] . "\n" . 'URL:' . $data['results']['shop']['0']['urls']['pc'];
-                    	$shopInfo1 = $data['results']['shop']['1']['name'] . "\n" . 'URL:' . $data['results']['shop']['1']['urls']['pc'];
-                    	$shopInfo2 = $data['results']['shop']['2']['name'] . "\n" . 'URL:' . $data['results']['shop']['2']['urls']['pc'];
-                    	$shopInfo3 = $data['results']['shop']['3']['name'] . "\n" . 'URL:' . $data['results']['shop']['3']['urls']['pc'];
-                    	$shopInfo4 = $data['results']['shop']['4']['name'] . "\n" . 'URL:' . $data['results']['shop']['4']['urls']['pc'];
-                    	$client->replyMessage([
-                        	'replyToken' => $event['replyToken'],
-                        	'messages' => [
-                             	[
-                                   'type' => 'text',
-                                   'text' => $shopInfo0
-                                ],
-                                [
-                                   'type' => 'text',
-                                   'text' => $shopInfo1
-                                ],
-                                [
-                                   'type' => 'text',
-                                   'text' => $shopInfo2
-                                ],
-                                [
-                                   'type' => 'text',
-                                   'text' => $shopInfo3
-                                ],
-                                [
-                                   'type' => 'text',
-                                   'text' => $shopInfo4
-                                ]
-                            ]
-                        ]);
-               	    } else {
-                    	$client->replyMessage([
-                        	'replyToken' => $event['replyToken'],
-                        	'messages' => [
-                          	   	[
-                               		    'type' => 'text',
-                                   	    'text' => '近くにお店がありません'
-                                	]
-				]
-			]);	
-		   }
-                    break;
+	switch ($event['type']) {
+		case 'message':
+			$message = $event['message'];
+			switch ($message['type']) {
+				case 'text':
+					$client->replyMessage([
+						'replyToken' => $event['replyToken'],
+						'messages' => [
+							[
+							'type' => 'text',
+							'text' => $message['text']
+							]
+						]
+					]);
+					break;
+				case 'location':
+					$lat = $message['latitude'];
+					$lng = $message['longitude'];
+					$hotUrl = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e8a77202e4c8db72&lat=' . $lat . '&lng=' . $lng . '&range=5&order=4&format=json';
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, $hotUrl);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					$result = curl_exec($ch);
+					curl_close($ch);
+					$data = json_decode($result, true);
+					if (!empty($data['results']['shop'])) {
+						$shopInfo0 = $data['results']['shop']['0']['name'] . "\n" . 'URL:' . $data['results']['shop']['0']['urls']['pc'];
+						$shopInfo1 = $data['results']['shop']['1']['name'] . "\n" . 'URL:' . $data['results']['shop']['1']['urls']['pc'];
+						$shopInfo2 = $data['results']['shop']['2']['name'] . "\n" . 'URL:' . $data['results']['shop']['2']['urls']['pc'];
+						$shopInfo3 = $data['results']['shop']['3']['name'] . "\n" . 'URL:' . $data['results']['shop']['3']['urls']['pc'];
+						$shopInfo4 = $data['results']['shop']['4']['name'] . "\n" . 'URL:' . $data['results']['shop']['4']['urls']['pc'];
+						$client->replyMessage([
+							'replyToken' => $event['replyToken'],
+							'messages' => [
+								[
+								'type' => 'text',
+								'text' => $shopInfo0
+								],
+								[
+								'type' => 'text',
+								'text' => $shopInfo1
+								],
+								[
+								'type' => 'text',
+								'text' => $shopInfo2
+								],
+								[
+								'type' => 'text',
+								'text' => $shopInfo3
+								],
+								[
+								'type' => 'text',
+								'text' => $shopInfo4
+								]
+							]
+						]);
+					} else {
+						$client->replyMessage([
+							'replyToken' => $event['replyToken'],
+							'messages' => [
+								[
+								'type' => 'text',
+								'text' => '近くにお店がありません'
+								]
+							]
+						]);	
+					}
+					break;
+				default:
+					$client->replyMessage([
+						'replyToken' => $event['replyToken'],
+						'messages' => [
+							[
+							'type' => 'text',
+							'text' => '位置情報かメッセージしか対応していません。'
+							]
+						]
+					]);	
+					break;
 		default:
-                    error_log('Unsupported message type: ' . $message['type']);
-                    break;
-                }
-        default:
-            error_log('Unsupported event type: ' . $event['type']);
-            break;
+			error_log('Unsupported event type: ' . $event['type']);
+			break;
     }
 };
