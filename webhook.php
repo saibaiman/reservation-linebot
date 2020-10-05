a8ac5ecd0d6ebb3244b3687ef55d113bfbe8901a
EC2 Default User <nagainozomi.19350511@gmail.com>
2020-10-05 07:40:13 +0000

    43

--- webhook.php
+++ webhook.php
@@ -21,13 +21,13 @@ require_once('./vendor/autoload.php');
 
 use Carbon\Carbon;
 
-try {
+/*try {
     $dbh = new PDO('mysql:host=localhost; dbname=procir_nagai127;charset=utf8;', 'nagai127', '2c7vcx1u47');
 } catch (PDOException $e) {
     echo '接続失敗:';
     exit;
 }
-
+*/
 $channelAccessToken = 'hy0mK6UwxH+2ooPGZpUr9mGknMfgOcRYZPwL7B5b5AMQGWVVdluOSsjveDfPlsu8riTNl45G0mJcpngdQ+oldHdqyVLSa15qR6H0naE+l5q7yf3ETynO7bV0PqmvZzcvg0fJEn5D/UFnkSo/QHv+rQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'c5f2a1532069465224d1183ac4256997';
 $postback = null;
